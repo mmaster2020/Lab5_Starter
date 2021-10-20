34832867e3f32faa9c95e7a944c726ff94146190
mmaster2020 <mmaster@ucsd.edu>
2021-10-20 16:14:28 -0700

    send .js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,14 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var typehorn = document.querySelector('#horn-select');
+  	//#typehorn is a reference to the tag with id horn-select in the html, i.e., the select on 31
+  var img_ref = document.querySelector('img');
+  if(typehorn.value == 'air-horn'){
+    img_ref.src = 'assets/images/air-horn.svg';
+  } else if (typehorn.value == 'car-horn'){
+    img_ref.src = 'assets/images/car-horn.svg';
+  } else if (typehorn.value == 'party-horn'){
+    img_ref.src = 'assets/images/party-horn.svg';
+  }
 }
